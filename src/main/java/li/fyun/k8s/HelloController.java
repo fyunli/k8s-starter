6be63e610e1ba09cfc0354529d0570b42a3c3471
fyunli <fyunli@gmail.com>
2019-05-25 23:15:01 +0800

    add logging

--- src/main/java/li/fyun/k8s/HelloController.java
+++ src/main/java/li/fyun/k8s/HelloController.java
@@ -1,5 +1,7 @@
 package li.fyun.k8s;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    Logger logger = LoggerFactory.getLogger(HelloController.class);
+
     @Value("${app.k8s.greeting}")
     private String greeting;
 
     @GetMapping("/hello")
     public String hello() {
+        logger.info("//: THE GREETING IS: {}", greeting);
         return greeting;
     }
 
